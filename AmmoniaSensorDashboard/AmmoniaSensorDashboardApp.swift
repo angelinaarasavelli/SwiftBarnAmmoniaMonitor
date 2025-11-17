b584af1e186177e25ee56a221a4c4c60d9468adf
Angelina Arasavelli <aarasavelli@wisc.edu>
2025-11-17 07:59:34 -0600

    Initial Commit

---
+++ AmmoniaSensorDashboard/AmmoniaSensorDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmmoniaSensorDashboardApp.swift
+//  AmmoniaSensorDashboard
+//
+//  Created by Angelina Arasavelli on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct AmmoniaSensorDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
